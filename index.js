eb90dea4fb0ff83c570b0b1c99008245c5384ab7
pournima108 <mishra.pournima108@gmail.com>
2018-04-26 09:48:11 +0530

    second commit

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
-var express=require('express');
-var bodyparser=require('body-parser');
+
 var square=require("./Methods/process");
-var app= express();
-console.log("hello")
 
-app.get(function(req,res){
 
-console.log(data.getHelloPhrase());
-console.log(data.getarea());
-console.log(data.getCustomerHelloPhrase());
+main()
 
-})
-
+function main(){
+    var data = new square(10);
+   console.log (data.getCustomerHelloPhrase());
+   console.log(data.getarea());
+   console.log(data.getHelloPhrase())
+}
 
 
