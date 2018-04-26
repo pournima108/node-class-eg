364ab527c31fd1d5fa400d4cbdd98318b812704a
pournima108 <mishra.pournima108@gmail.com>
2018-04-26 09:28:14 +0530

    first commit

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var app= express();
 console.log("hello")
 
 app.get(function(req,res){
-var data = new square(10);
+
 console.log(data.getHelloPhrase());
 console.log(data.getarea());
 console.log(data.getCustomerHelloPhrase());
